97d7a83ec93d3f0e09c8499596856b974ab1cfdb
Filippo <filippo.cavallari99@gmail.com>
2023-05-27 16:35:56 +0200

    Make flask public

--- client/src/main/scala/MainDemo.scala
+++ client/src/main/scala/MainDemo.scala
@@ -1,2 +1,8 @@
+import it.pps.ddos.deployment.Deployer
+
+import java.net.InetAddress
+
 object MainDemo extends App:
+    val serverAddress: String = InetAddress.getLocalHost.getHostAddress
+    Deployer.createActorSystem(serverAddress)
     ClientView()
