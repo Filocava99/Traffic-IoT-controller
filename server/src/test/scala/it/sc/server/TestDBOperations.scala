8bfac8310ef8d7f9aa716239b643a4794cc123b0
AndreaIngargiola <92037242+AndreaIngargiola@users.noreply.github.com>
2023-06-04 18:11:08 +0200

    clean from comments

--- server/src/test/scala/it/sc/server/TestDBOperations.scala
+++ server/src/test/scala/it/sc/server/TestDBOperations.scala
@@ -23,26 +23,10 @@ import it.sc.server.ServerActor
 import org.bson.types.ObjectId
 
 class TestDBOperations extends AnyFlatSpec:
-  //"A DBWriter" should "work" in testDBWriter()
   "The management of a new camera id" should "be done properly" in testCameraInit()
-  /*"A MongoDBFind" should "returns all the data in the database" in testMongoDBFindAll()
-  "A MongoDBFind" should "returns only the data in the database that match the specified ID" in testMongoDBFindByID()*/
 
   val testKit: ActorTestKit = ActorTestKit()
   private var testProbe = testKit.createTestProbe[DeviceMessage]()
-
-//  def testDBWriter(): Unit =
-//    val date = DateTime.now()
-//    DBWriter(99, Map(date -> Set((0,0),(6,2),(3,2)), date + 3.seconds -> Set((0,322))))
-//    while(true) {}
-
- /* def testMongoDBFindAll(): Unit =
-    MongoDBFind()
-    Thread.sleep(10000) // waiting for the read operation to complete
-
-  def testMongoDBFindByID(): Unit =
-    MongoDBFind(99)
-    Thread.sleep(10000) // waiting for the read operation to complete*/
 
   def testCameraInit(): Unit =
     val serverRef = testKit.spawn(ServerActor())
